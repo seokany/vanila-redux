4e9df6212ea899f015ee543e42e677cd50c4e097
soekany <seokhwan.h.yi@gmail.com>
2020-04-20 16:35:40 +0900

    1.2

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Learning Vanilla Redux and React-Redux
 
 - [x] 1.1 Vanilla Counter 
 js를 이용하여 add, minus handler로 counter 만들기.
-- [ ]  1.2 Store and Reducer
+- [x]  1.2 Store and Reducer
+Store? Reducer? state?
 - [ ]  1.3 Actions
 - [ ]  1.4 Subscriptions
 - [ ]  1.5 Recap Refactor
--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
-// 1. id = add, minus span = count만들가.
+import { createStore } from "redux";
+
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
-
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerHTML = 0;
 
-const updateText = () => {
-  number.innerText = count;
+// reducer 즉  countModifier는 data를 변화 시킬 수 있는 유일한 곳.
+const countModifier = (count=0) => {
+  // count = 0 initail state
+  // count가 state 이다. 즉, 데이터의 상태를 나타내는 변수. 
+  return count;
 }
+// store 즉, countStore는 data를 저장하는 곳. 
+// createStore()라는 함수로 저장소를 만든다. 오직 이곳에서만 데이터를 저장할 수 있다.
+const countStore = createStore(countModifier);
 
-const handleAdd = () => {
-  // console.log("Add count")
-  count ++;
-  // number.innerHTML = count;
-  updateText();
-}
+console.log(countStore.getState())
 
-const handleMinus = () => {
-  // console.log("Minus count")
-  count --;
-  // number.innerHTML = count;
-  updateText();
-}
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
 
