953f0a6f48427b509371182010ec00bedcf8b66a
soekany <seokhwan.h.yi@gmail.com>
2020-04-20 20:15:29 +0900

    v1.4

--- README.md
+++ README.md
@@ -2,11 +2,8 @@
 
 Learning Vanilla Redux and React-Redux
 
-- [x] 1.1 Vanilla Counter 
->  js를 이용하여 add, minus handler로 counter 만들기.
-- [x]  1.2 Store and Reducer
-> Store? Reducer? state?
-- [x]  1.3 Actions
-> action? dispatch()? getState()?
-- [ ]  1.4 Subscriptions
+- [x] 1.1 Vanilla Counter : js를 이용하여 add, minus handler로 counter 만들기.
+- [x]  1.2 Store and Reducer : Store? Reducer? state?
+- [x]  1.3 Actions : action? dispatch()? getState()?
+- [x]  [1.4 Subscriptions](https://redux.js.org/api/store#subscribelistener) : subscript(), onChange(), addEventLis
 - [ ]  1.5 Recap Refactor
--- src/index.js
+++ src/index.js
@@ -1,34 +1,41 @@
 import { createStore } from "redux";
 
-// const add = document.getElementById("add");
-// const minus = document.getElementById("minus");
-// const number = document.querySelector("span");
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// dispatch() 메서드를 통해서 action이 실행된다.
-// action은 object 형태로만 받는다.
+number.innerText = 0;
 
-let addObject = {type: "Add"};
-let minusObject = {type: "Minus"}
-
-const countModifier = (count=0, action) => {
-  console.log(count, action);
-  if(addObject.type === "Add" ) {
+const countModifier = (count = 0, action) => {
+  // console.log(count, action);
+  if(action.type === "ADD" ) {
     return count + 1;
-  } else if (minusObject.type === "Minus") {
-    return count - 1;
+  } else if (action.type === "MINUS") {
+    return count  - 1;
   } 
   return count;
 }
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch(addObject);
-countStore.dispatch(addObject);
-countStore.dispatch(addObject);
-countStore.dispatch(minusObject);
+const onChange = () => {
+  number.innerText = countStore.getState();
+  // console.log(countStore.getState())
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type: "ADD"});
+};
+const handleMinus = () => {
+  countStore.dispatch({type:"MINUS"})
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 
 
-console.log(countStore.getState())
 
 
 
