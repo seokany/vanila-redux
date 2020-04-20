f85ae59363dca06ed67b6d43fa66008a682ab76f
soekany <seokhwan.h.yi@gmail.com>
2020-04-20 18:19:36 +0900

    1.3

--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 Learning Vanilla Redux and React-Redux
 
 - [x] 1.1 Vanilla Counter 
-js를 이용하여 add, minus handler로 counter 만들기.
+>  js를 이용하여 add, minus handler로 counter 만들기.
 - [x]  1.2 Store and Reducer
-Store? Reducer? state?
-- [ ]  1.3 Actions
+> Store? Reducer? state?
+- [x]  1.3 Actions
+> action? dispatch()? getState()?
 - [ ]  1.4 Subscriptions
 - [ ]  1.5 Recap Refactor
--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 import { createStore } from "redux";
 
+// const add = document.getElementById("add");
+// const minus = document.getElementById("minus");
+// const number = document.querySelector("span");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+// dispatch() 메서드를 통해서 action이 실행된다.
+// action은 object 형태로만 받는다.
 
+let addObject = {type: "Add"};
+let minusObject = {type: "Minus"}
 
-// reducer 즉  countModifier는 data를 변화 시킬 수 있는 유일한 곳.
-const countModifier = (count=0) => {
-  // count = 0 initail state
-  // count가 state 이다. 즉, 데이터의 상태를 나타내는 변수. 
+const countModifier = (count=0, action) => {
+  console.log(count, action);
+  if(addObject.type === "Add" ) {
+    return count + 1;
+  } else if (minusObject.type === "Minus") {
+    return count - 1;
+  } 
   return count;
 }
-// store 즉, countStore는 data를 저장하는 곳. 
-// createStore()라는 함수로 저장소를 만든다. 오직 이곳에서만 데이터를 저장할 수 있다.
+
 const countStore = createStore(countModifier);
+
+countStore.dispatch(addObject);
+countStore.dispatch(addObject);
+countStore.dispatch(addObject);
+countStore.dispatch(minusObject);
+
 
 console.log(countStore.getState())
 
