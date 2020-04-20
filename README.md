306aef37d3b4b447abc3a3a0a68092ae441a2d47
soekany <seokhwan.h.yi@gmail.com>
2020-04-20 21:47:18 +0900

    v2.0

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 Learning Vanilla Redux and React-Redux
 
+- [x] 2.0 Vanilla ToDo
+- [] 2.1 Vanilla ToDo Refactor
+- [] 2.2 State Mutation
+- [] 2.3 Delete To Do
+- [] 2.4 Delete To Do part Two
+- [] 2.5 Conclusions
+
+
+
+----
+END.
 - [x] 1.1 Vanilla Counter : js를 이용하여 add, minus handler로 counter 만들기.
 - [x]  1.2 Store and Reducer : Store? Reducer? state?
 - [x]  1.3 Actions : action? dispatch()? getState()?
--- src/index.js
+++ src/index.js
@@ -1,45 +1,23 @@
-import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-const ADD = "ADD"
-const MINUS = "MINUS"
-
-const countModifier = (count = 0, action) => {
-  // console.log(count, action);
-  switch(action.type) {
-    case ADD:
-      return count + 1
-    case MINUS:
-      return count - 1;
-    default:
-      return count
-  }
+const createTodo = todo => {
+  const li = document.createElement("li");
+  li.innerText = todo;
+  ul.appendChild(li);
 }
 
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-  // console.log(countStore.getState())
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  if(toDo === ""){
+    alert("Do anything")
+  } else if(toDo) {
+    createTodo(toDo)
+  }
+  // createTodo(toDo);
 };
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({type: "ADD"});
-};
-const handleMinus = () => {
-  countStore.dispatch({type:"MINUS"})
-};
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
-
-
-
-
+form.addEventListener("submit", onSubmit);
